c98249ca953adeb4f284c3b6036b2910bb335385
Matthew-Andrews03 <matthewa0903@gmail.com>
2024-03-04 12:26:25 -0500

    signed division test

--- div32.v
+++ div32.v
@@ -8,11 +8,26 @@ module div32(
 	reg [31:0] Q;
 	reg [31:0] M;
 	reg q0;
+   reg a;
+   reg q;
+
 
    always @ (*) begin
       Q = RegA;
       M = RegB;
       A = 32'b0;
+      a = 1'b0;
+      q = 1'b0;
+
+      if(Q[31] == 1'b1) begin
+         negate32(Q, Q);
+         a <= 1;
+      end
+
+      else if(M[31] == 1'b1) begin
+         negate32(M, M);
+         q <= 1;
+      end
 
       for (i = 0; i < 32; i = i + 1) begin
          {A, Q} = {A, Q} << 1;
@@ -27,6 +42,42 @@ module div32(
       end
    end
 
+   if(a != q) begin
+      negate32(Q, Q);
+   end
+
    assign Z = {A, Q};
 
 endmodule
+
+
+// module div32(
+//    input wire signed [31:0] RegA,
+//    input wire signed [31:0] RegB,
+//    output wire signed [63:0] Z
+// );
+//    integer i;
+//    reg signed [31:0] A;
+//    reg signed [31:0] Q;
+//    reg signed [31:0] M;
+//    reg q0;
+
+//    always @ (*) begins
+//       Q = RegA;
+//       M = RegB;
+//       A = 32'sb0;
+
+//       for (i = 0; i < 32; i = i + 1) begin
+//          {A, Q} = {A, Q} << 1;
+//          A = A - M;
+//          if (A[31] == 1'b0) begin
+//                Q[0] = 1;
+//          end
+//          else if (A[31] == 1'b1) begin
+//                Q[0] = 0;
+//                A = A + M;
+//          end
+//       end
+//    assign Z = {A, Q};
+
+// endmodule
